342caf1dd6815a460fe9a495e85ccd65031b56fc
Marannnn <martin.hrubes1@seznam.cz>
2024-10-06 11:19:36 +0200

    Add Js
    Prejmenovali jsme a pridali javascript

---
+++ Frontend/browse-mode/browse.js
@@ -0,0 +1,23 @@
+data = {}
+
+d = {
+    "name": "Achát",
+    "aliases": [
+        "achat"
+    ],
+    "paths": [
+        "achat.jpg"
+    ]
+}
+
+// url = "county-contracts.gl.at.ply.gg" // TODO: udelat adresu
+
+// async function getData()R {
+//     res = fetch(url + "/whole");
+//     data = await res.json();
+//     console.log(data);
+// }
+
+// getData()
+
+
